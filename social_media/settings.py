08275dd72f87264e3114f86c7ffe05919cb4ace7
AibaisAibek <aiba.mak94@gmail.com>
2021-04-12 23:45:20 +0600

    f com

--- social_media/settings.py
+++ social_media/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'social_media.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'three',
+        'USER': 'aibek3',
+        'PASSWORD': 'aibek3',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
